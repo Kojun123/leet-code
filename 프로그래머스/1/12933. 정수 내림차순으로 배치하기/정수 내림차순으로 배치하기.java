5419451716cbb9e785cdc13c176a4d7b0647163f
Ojun Kwon <124665643+Kojun123@users.noreply.github.com>
2025-02-14 21:34:30 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 1.18 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        String[] nn = String.valueOf(n).split("");
+        Arrays.sort(nn, Collections.reverseOrder());
+        answer = Long.parseLong(String.join("", nn));
+        return answer;
+    }
+}
