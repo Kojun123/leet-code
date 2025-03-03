2017defc03f142b7e5754b4fdd8692b82d62aa27
Ojun Kwon <124665643+Kojun123@users.noreply.github.com>
2025-03-03 19:42:07 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 33.55 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        List<Integer> result = new ArrayList<>();
+        
+        for(int i=0; i<arr.length; i++) {
+            if(i == 0 || arr[i-1] != arr[i]){
+                 result.add(arr[i]);
+            }
+        }
+
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+}
